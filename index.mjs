128ec0d0ddd81c2cfd48d482ea543ed983fb2697
wine <eithinzarphyo430@gmail.com>
2025-01-09 16:04:02 +0630

    Update changes

--- index.mjs
+++ index.mjs
@@ -1,1 +1,5 @@
 console.log("HEllo World")
+import {add,minus,PI} from "./math.mjs"
+console.log(add(5,6))
+console.log(PI)
+console.log(minus(5,1))
---
+++ math.mjs
@@ -0,0 +1,8 @@
+const PI=3.14
+function add(a,b){
+    return a+b
+}
+function minus(a,b){
+    return a-b
+}
+export {add,PI,minus}
